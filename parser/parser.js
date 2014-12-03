2a0dfc8102124c161e8150d0a1b8bd4b8dc42088
Dave Mun <mun.dave@gmail.com>
2014-12-03 15:21:39 -0800

    Bugfix for parser.js.

--- parser/parser.js
+++ parser/parser.js
@@ -1,26 +1,5 @@
-exports.parse = function(req, res){
-  res.send("good attempt!");
+module.exports = {
+  parse : function(req, res){
+            res.send("good attempt!")
+          }
 }
-
-[{func: 'add',
-  tests:[{
-    args:[1,2],
-    ans: 3
-    },
-    {
-    args:[4,5],
-    ans: 9
-    },
-    {
-    args:[1,2],
-    ans: 3
-    },
-    {
-    args:[-4,2],
-    ans: -2
-    },
-    {
-    args:[-4,-9],
-    ans: -13
-    }]
-  }];
