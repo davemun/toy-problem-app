ee8575eb862670392d2c9c97cb330936e12c2131
Rory <roryc89@googlemail.com>
2014-12-03 22:50:40 +0000

    '2 test scaffolds created'

--- parser/parser.js
+++ parser/parser.js
@@ -1,3 +1,26 @@
 exports.parse = function(req, res){
   res.send("good attempt!");
 }
+
+[{func: 'add',
+  tests:[{
+    args:[1,2],
+    ans: 3
+    },
+    {
+    args:[4,5],
+    ans: 9
+    },
+    {
+    args:[1,2],
+    ans: 3
+    },
+    {
+    args:[-4,2],
+    ans: -2
+    },
+    {
+    args:[-4,-9],
+    ans: -13
+    }]
+  }];
