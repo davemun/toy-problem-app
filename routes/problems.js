d83e31a62b53be6244a4efd6c7a1ecf67855ff7e
Dave Mun <mun.dave@gmail.com>
2014-12-03 12:55:45 -0800

    Added example toy problem 1.

--- routes/problems.js
+++ routes/problems.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
+router.get('/*', function(req, res) {
+  res.render('problem'+req.path.slice(1), { title: 'Toy Problem '+req.path.slice(1) });
+});
+
 module.exports = router;
