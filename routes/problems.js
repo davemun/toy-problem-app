7d3420a1b710eff3c36315200b30c4c97992bc06
Rory <roryc89@googlemail.com>
2014-12-04 19:36:36 +0000

    SpiralTraversal description and test implemented

--- routes/problems.js
+++ routes/problems.js
@@ -26,7 +26,8 @@ router.post('/*', function(req, res){
 
 var nameDescTups = [
                      ['add', 'Create an addition function that returns the sum of the two arguments'],
-                     ['isPrime', 'Create a function that returns true if a number is prime and false if it is not']
+                     ['isPrime', 'Create a function that returns true if a number is prime and false if it is not'],
+                     ['spiralTraversal', 'Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),\n and prints out every value found, but in a spiral from the upper left in to the center\n Please write some form of specs, tests, or assertions for your code, and check as many edge cases as you can think of']
                    ];
 
 
