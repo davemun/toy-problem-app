51b0a10009cf3e2585ebd429e198f7982204eab8
Rory <roryc89@googlemail.com>
2014-12-04 18:17:42 +0000

    'result alert message'

--- public/models/problem-model.js
+++ public/models/problem-model.js
@@ -1,26 +1,41 @@
-$("#solutionform").submit(function(e)
-{
-    var postData = $(this).serializeArray();
-    var formURL = "/problems";
-    $.ajax(
-    {
-        url : formURL,
-        type: "POST",
-        data : postData,
-        success:function(data, textStatus, jqXHR)
-        {
-            console.log('success')
-            alert(data)
-            console.log(data);
-        },
-        error: function(jqXHR, textStatus, errorThrown)
-        {
-            console.log('error')
-        }
-    });
-    e.preventDefault(); //STOP default action
-    //e.unbind(); //unbind. to stop multiple form submit.
+'use strict'
+
+$(function(){
+
+  function showResult(data){
+
+    console.log('showResult')
+
+    // bootbox.alert(data[1]);
+    $('#myModal').modal('show');
+
+  }
+
+  $("#solutionform").submit(function(e)
+  {
+      var postData = $(this).serializeArray();
+      var formURL = "/problems";
+      $.ajax(
+      {
+          url : formURL,
+          type: "POST",
+          data : postData,
+          success:function(data, textStatus, jqXHR)
+          {
+            showResult(data);
+            console.log('success');
+
+          },
+          error: function(jqXHR, textStatus, errorThrown)
+          {
+              console.log('error')
+          }
+
+      });
+      e.preventDefault(); //STOP default action
+      //e.unbind(); //unbind. to stop multiple form submit.
+  });
+
+
+
 });
-
-
- var url = "/problems";
