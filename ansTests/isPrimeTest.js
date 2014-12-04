10b3b021ce868c5f4bdb699b7a9cd87b2fbaa3bd
Rory <roryc89@googlemail.com>
2014-12-04 19:28:47 +0000

    spiral test added

---
+++ ansTests/isPrimeTest.js
@@ -0,0 +1,43 @@
+
+var test2 = {};
+
+test2.r1 = Math.random();
+test2.r2 = Math.random();
+
+test2.rAns = test2.r1 + test2.r2;
+
+test2.tests = [{
+    args:[1],
+    ans: false
+  },
+  {
+    args:[2],
+    ans: true
+  },
+  {
+    args:[3],
+    ans: true
+  },
+  {
+    args:[9],
+    ans: false
+  },
+  {
+    args:[67],
+    ans: true
+  },
+  {
+    args:[181],
+    ans: true
+  },
+  {
+    args:[301],
+    ans: false
+  },
+  {
+    args:[479],
+    ans: true
+  }
+];
+
+module.exports = test2;
