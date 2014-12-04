2e1c77dcf1e45a6556ddcf877ce668dfb4be939b
Dave Mun <mun.dave@gmail.com>
2014-12-03 16:58:31 -0800

    Separated syntax and answer handling.

--- parser/parser.js
+++ parser/parser.js
@@ -2,10 +2,11 @@ module.exports = {
   parse : function(req, res){
             try{    
                 eval(req.body.answer);
-                var result = req.body.funcName(1,1), expectedResult = 2;
+                var result = eval('req.body.funcName(1,1)'), expectedResult = 2;
                 console.log("funcName: "+req.body.funcName);
             }catch(err){
                  res.send("syntax error!");
+                 return;
             };
             if(result != expectedResult){
                 res.send("wrong answer!");
