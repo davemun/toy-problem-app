5ef4a93032cff0287c3e82036a231f5ec7d5234a
Dave Mun <mun.dave@gmail.com>
2014-12-04 13:12:01 -0800

    Fixed unit test alerts.

--- parser/parser.js
+++ parser/parser.js
@@ -4,43 +4,33 @@ module.exports = {
             var testPage = require('../ansTests/'+req.body.funcName+'Test.js');
             var testProgress = [], successful = 0;
 
+            for(var i = 0; i < testPage.tests.length; i++){
+              try{
+                  eval(req.body.answer);
 
-            try{
-                eval(req.body.answer);
-
-                for(var i = 0; i < testPage.tests.length; i++){
                   var result = eval(req.body.funcName+'.apply(null,'+JSON.stringify(testPage.tests[i].args)+')'),
                       expectedResult = testPage.tests[i].ans;
 
                   if(result !== expectedResult){
-                      testProgress.push([testPage.tests[i].args.toString(),result,"failure"]);
+                      testProgress.push([testPage.tests[i].args.toString(),result,expectedResult,"failure"]);
                   }else{
-                      testProgress.push([testPage.tests[i].args.toString(),result,"success"]);
+                      testProgress.push([testPage.tests[i].args.toString(),result,expectedResult,"success"]);
                   }
-                }
-
-            }catch(err){
-                 var status = "error";
-                 testProgress[2] = "syntax error!";
-                 res.send(
-                  { testProgress: testProgress,
-                    message: "You have a syntax error!",
-                    status: status
-                  }
-                 );
-                 return;
-            };
+              }catch(err){
+                   var status = "error";
+                    testProgress.push([testPage.tests[i].args.toString(),"Syntax Error!",expectedResult,"error"]);
+              };
+            }
 
             //if they made it here all the tests completed without eval error
             for(var i = 0; i < testProgress.length; i++){
-              if(testProgress[i][2] === "success"){
+              if(testProgress[i][3] === "success"){
                 successful++;
               }
             }
 
-            var status = (successful == testPage.tests.length) ? "success" : "failure";
-            console.log(status);
-
+            var status = (successful === testPage.tests.length) ? "success" : "incomplete";
+            
             res.send(
               { testProgress: testProgress,
                 message: "You passed "+successful+" out of "+testPage.tests.length+" tests!",
@@ -50,3 +40,4 @@ module.exports = {
 
           }
 };
+//0: null1: null2: "syntax error!
--- public/models/problem-model.js
+++ public/models/problem-model.js
@@ -10,19 +10,44 @@ $("#solutionform").submit(function(e)
         data : postData,
         success:function(data, textStatus, jqXHR)
         {
+            //empty out unit tests div  
+            $('.testResults').html("");
+            
+            //hide all overall progress alerts
+            $('.problem-success').hide();
+            $('.problem-incomplete').hide();
+
+            //show relevant progress alert
             if(data.status === "success"){
                 $('.problem-success').text(data.message).show();
-                $('.problem-failure').hide();
-                $('.problem-error').hide();
-            }else if(data.status === "failure"){
-                $('.problem-success').hide();
-                $('.problem-failure').text(data.message).show();
-                $('.problem-error').hide();
-            }else if(data.status === "error"){
-                $('.problem-success').hide();
-                $('.problem-failure').hide();
-                $('.problem-error').text(data.message).show();
+            }else if(data.status === "incomplete"){
+                $('.problem-incomplete').text(data.message).show();
             }
+
+            
+            //go through each unit test result and create+append that result to the test result section
+            data.testProgress.forEach(function(testResult){
+
+                var arguments = $('<div><strong>Input Arguments:</strong> '+testResult[0]+'</div>');
+                var result = $('<div><strong>Actual Result:</strong> '+testResult[1]+'</div>');
+                var expectedResult = $('<div><strong>Expected Result:</strong> '+testResult[2]+'</div>');
+                var status = $('<div><strong>Status:</strong> '+testResult[3]+'</div>');
+
+                var alert = $('<div class="alert  alert-dismissible" role="alert"></div>');
+
+                if(testResult[3] === "success"){
+                    $(alert).addClass("alert-success").addClass("problem-success");
+                }else if(testResult[3] === "failure"){
+                    $(alert).addClass("alert-warning").addClass("problem-failure");
+                }else if(testResult[3] === "error"){
+                    $(alert).addClass("alert-danger").addClass("problem-error");
+                }
+
+                $(alert).append([arguments, result, expectedResult, status]);
+                $(alert).appendTo('.testResults');
+
+            });
+
         },
         error: function(jqXHR, textStatus, errorThrown)
         {
@@ -32,4 +57,3 @@ $("#solutionform").submit(function(e)
     e.preventDefault(); //STOP default action
     //e.unbind(); //unbind. to stop multiple form submit.
 });
-
