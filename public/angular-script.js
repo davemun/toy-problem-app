1c0f1650c07a1f9bacd95bf076d62a70533f5af4
Dave Mun <mun.dave@gmail.com>
2015-01-27 11:04:48 -0800

    Pages routed into angular partials using factories.

--- public/angular-script.js
+++ public/angular-script.js
@@ -7,42 +7,33 @@ app.config(function($routeProvider) {
     $routeProvider
         // route for the home page
         .when('/', {
-            templateUrl : 'pages/home.html',
+            templateUrl : 'pages/partials/home.html',
+        })
+
+        // route for the problems
+        .when('/problems/:route', {
+            templateUrl: 'pages/partials/problems.html',
             controller  : 'mainController'
         })
+});
 
-        // route for the addition toy problem
-        .when('/addition', {
-            templateUrl : 'pages/partials/addTest.html',
-            controller  : 'addTestController'
-        })
-
-        // route for the addition toy problem
-        .when('/primeTester', {
-            templateUrl : 'pages/partials/addTest.html',
-            controller  : 'addTestController'
-        })
-
-        // route for the addition toy problem
-        .when('/spiralTraversal', {
-            templateUrl : 'pages/partials/addTest.html',
-            controller  : 'addTestController'
-        })
-
-        // route for the addition toy problem
-        .when('/allAnagrams', {
-            templateUrl : 'pages/partials/addTest.html',
-            controller  : 'addTestController'
-        })
-
+//expose $location in $rootScope
+app.run(function($rootScope, $location) {
+    $rootScope.location = $location;
 });
 
 // create the controller and inject Angular's $scope
-app.controller('mainController', function($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
-});
+app.controller('mainController', ['$scope', '$location', 'Descriptions', function($scope, $location, Descriptions) {
+    var problemName = $location.path().split('/').pop();
+    $scope.name = problemName;
+    $scope.desc = Descriptions[problemName];
+}]);
 
-app.controller('addTestController', function($scope) {
-    $scope.message = 'This is the addTest page.';
+app.factory('Descriptions', function($http) {
+    return {
+      addition: "Create an addition function that returns the sum of the two arguments",
+      primeTester: "Create a function that returns true if a number is prime and false if it is not",
+      spiralTraversal: "Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),\n and prints out every value found, but in a spiral from the upper left in to the center.",
+      allAnagrams: "Given a single input string, write a function that produces all possible anagrams of a string and outputs them as an array."
+    }
 });
